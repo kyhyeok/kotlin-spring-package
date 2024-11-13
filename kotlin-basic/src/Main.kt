ab0f3befb21982b50fdc17718da013116ce327ae
김윤혁 <kyhyeok@gmail.com>
2024-11-13 18:46:26 +0900

    입문자를 위한 Kotlin 문법
    코틀린 기초
    02. 변수

---
+++ kotlin-basic/src/Main.kt
@@ -0,0 +1,24 @@
+var x = 5;
+// 함수 혹은 클래스 밖에서 변수 설정 가능 (탑 레벨에 위치 한다)
+fun main() {
+    x +=1
+    println(x)
+    val a : Int = 1
+
+    val b = 1
+
+    val c : Int
+    c = 3
+
+//    val d
+//    d = 123
+
+    //val(value) 불변
+    //var(variable) 가변 변수
+
+//    val e : String = "Hello"
+//    e = "World" 컴파일 오류
+
+//    var f = 123 Int
+//    f = "World" String 컴파일 오류
+}
